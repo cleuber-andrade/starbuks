041ce2b2a7eb58740f2b8c6fc2acd6b2671729fc
cleuber-andrade <cleuber_binho@hotmail.com>
2021-08-12 13:38:08 -0300

    update

---
+++ assets/js/animation.js
@@ -0,0 +1,8 @@
+function imgSlider(imagem){
+    document.querySelector('.starbucks').src = imagem;    
+}
+
+function changeCircle(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
